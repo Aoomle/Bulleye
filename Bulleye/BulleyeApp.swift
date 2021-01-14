3141a87401b38f1f47cd3ebc58e4e98c2a6b91c8
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-01-14 02:08:15 +0100

    Initial Commit

---
+++ Bulleye/BulleyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BulleyeApp.swift
+//  Bulleye
+//
+//  Created by Abdulmalik Muhammad on 13/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct BulleyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
