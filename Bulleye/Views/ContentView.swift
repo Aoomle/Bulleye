245bcebe78a29b5757371fec1dfbc3a3b8278344
Abdulmalik Muhammad <bawasuru@gmail.com>
2021-01-21 02:12:32 +0100

    challenge #1 completed #100DaysOfCode

--- Bulleye/Views/ContentView.swift
+++ Bulleye/Views/ContentView.swift
@@ -9,62 +9,81 @@ import SwiftUI
 
 struct ContentView: View {
   
-@State private var alertIsVisible = false
-@State private var sliderValue = 10.0
-@State private var game = Game()
+  @State private var alertIsVisible = false
+  @State private var sliderValue = 10.0
+  @State private var game = Game()
   
   var body: some View {
     ZStack {
       Color("BackgroundColor")
         .edgesIgnoringSafeArea(.all)
       VStack {
-        Text("🎯🎯🎯\nPut the bulleye as close as you can to".uppercased())
-          .bold()
-          .kerning(2.0)
-          .multilineTextAlignment(.center)
-          .lineSpacing(4.0)
-          .font(.footnote)
-          .foregroundColor(.init("TextColor"))
-        
-        Text("\(game.target)")
-          .kerning(-1.0)
-          .foregroundColor(.init("TextColor"))
-          .font(.largeTitle)
-          .fontWeight(.black)
-        
-        HStack {
-          Text("1")
-            .foregroundColor(.init("TextColor"))
-            .bold()
-          Slider(value: $sliderValue, in: 1.0...100.0)
-          Text("100")
-            .foregroundColor(.init("TextColor"))
-            .bold()
-        }
-        .padding()
-        Button(action: {
-          alertIsVisible = true
-        }) {
-          Text("Hit Me".uppercased())
-            .foregroundColor(.init("TextColor"))
-            .bold()
-            .font(.title3)
-        }
-        .padding(20.0)
-        .background(
-          ZStack {
-            Color("ButtonColor")
-            LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
-          }
-        )
-        .foregroundColor(.white)
-        .cornerRadius(15)
-        .alert(isPresented: $alertIsVisible, content: {
-          let roundedValue = Int(sliderValue.rounded())
-          return Alert(title: Text("Hello There!"), message: Text("The slider value is:\(roundedValue)\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
-        })
-
+        InstructionView(game: $game)
+        SliderView(sliderValue: $sliderValue)
+        HitMeButton(alertIsVisible: $alertIsVisible, game: $game, sliderValue: $sliderValue)
       }
+    }
+  }
+}
+
+
+struct SliderView: View {
+  @Binding var sliderValue: Double
+  
+  var body: some View {
+    VStack {
+      HStack {
+        SliderLabelText(text: "1")
+        Slider(value: $sliderValue, in: 1.0...100.0)
+        SliderLabelText(text: "100")
+      }
+      .padding()
+    }
+  }
+}
+
+struct HitMeButton: View {
+  @Binding var alertIsVisible: Bool
+  @Binding var game: Game
+  @Binding var sliderValue: Double
+  
+  var body: some View {
+    VStack {
+      Button(action: {
+        alertIsVisible = true
+      }) {
+        Text("Hit Me".uppercased())
+          .foregroundColor(.init("TextColor"))
+          .bold()
+          .font(.title3)
+      }
+      .padding(20.0)
+      .background(
+        ZStack {
+          Color("ButtonColor")
+          LinearGradient(gradient: Gradient(colors: [Color.white.opacity(0.3), Color.clear]), startPoint: .top, endPoint: .bottom)
+        }
+      )
+      .foregroundColor(.white)
+      .cornerRadius(15)
+      .alert(isPresented: $alertIsVisible, content: {
+        let roundedValue = Int(sliderValue.rounded())
+        return Alert(title: Text("Hello There!"), message: Text("The slider value is:\(roundedValue)\n" + "You scored \(game.points(sliderValue: roundedValue)) points this round"), dismissButton: .default(Text("Awesome!")))
+      })
+    }
+  }
+}
+
+
+struct InstructionView: View {
+  @Binding var game: Game
+  
+  var body: some View {
+    VStack {
+      InstructionText(text: "🎯🎯🎯\nPut the bulleye as close as you can to")
+        .padding(.leading, 30.0)
+        .padding(.trailing, 30.0)
+      BigNumberText(text: "\(game.target)")
     }
   }
 }
@@ -79,3 +98,4 @@ struct ContentView_Previews: PreviewProvider {
       .previewLayout(.fixed(width: 568, height: 320))
   }
 }
+
---
+++ Bulleye/Views/TextViews.swift
@@ -0,0 +1,53 @@
+//
+//  TextViews.swift
+//  Bulleye
+//
+//  Created by Abdulmalik Muhammad on 21/01/2021.
+//
+
+import SwiftUI
+
+struct InstructionText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text.uppercased())
+      .bold()
+      .kerning(2.0)
+      .multilineTextAlignment(.center)
+      .lineSpacing(4.0)
+      .font(.footnote)
+      .foregroundColor(.init("TextColor"))
+  }
+}
+
+struct BigNumberText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .kerning(-1.0)
+      .foregroundColor(.init("TextColor"))
+      .font(.largeTitle)
+      .fontWeight(.black)
+  }
+}
+
+struct SliderLabelText: View {
+  var text: String
+  
+  var body: some View {
+    Text(text)
+      .foregroundColor(.init("TextColor"))
+      .bold()
+  }
+}
+
+struct InstructionText_Preview: PreviewProvider {
+  static var previews: some View {
+    VStack {
+      InstructionText(text: "Insturctions")
+      BigNumberText(text: "999")
+    }
+  }
+}
